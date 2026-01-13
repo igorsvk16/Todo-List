28106845f5026c0fd87d7618c7bb323b21096e0f
Igor <igorsvk16@gmail.com>
2026-01-13 12:34:20 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import { init, addProject, addTodo, getState } from "./modules/app";
+import { init } from "./modules/app";
 
 init();
-console.log("STATE AFTER INIT:", getState());
-
-const newProjectId = addProject("work");
-addTodo(newProjectId, { title: "finish", priority: "high" });
-
-console.log("STATE AFTER CHANGES:", getState());
